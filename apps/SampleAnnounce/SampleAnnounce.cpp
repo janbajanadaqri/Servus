d6d0debae96ec1f11fabd37083b5ee2a981b6d49
Jan Bajana <jan.bajana@daqri.com>
2018-02-21 17:22:20 +0100

    Sample annoucement and browser apps first versions.

--- apps/SampleAnnounce/SampleAnnounce.cpp
+++ apps/SampleAnnounce/SampleAnnounce.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 
 #include <cassert>
-#include <chrono>         // std::chrono::seconds
-#include <thread>         // std::this_thread::sleep_for
+#include <chrono> // std::chrono::seconds
+#include <thread> // std::this_thread::sleep_for
 
 #include <servus/listener.h>
 #include <servus/servus.h>
@@ -21,128 +21,131 @@
 #define _sleep ::sleep
 #endif
 
-namespace {
+namespace
+{
+static const int _propagationTime = 1000;
+static const int _propagationTries = 20;
 
-    static const int _propagationTime = 1000;
-    static const int _propagationTries = 20;
+void printHosts(const servus::Servus& service);
+void test(const std::string& serviceType, const std::string& instanceName,
+          const std::string& UUID, uint32_t inPort);
 
-    void printHosts(const servus::Servus& service);
-    void test(const std::string& serviceType, const std::string& instanceName,
-              const std::string& UUID, uint32_t inPort);
+void printHosts(const servus::Servus& service)
+{
+    servus::Strings instances = service.getInstances();
 
-    void printHosts(const servus::Servus& service)
+    std::cout << "Service type: " << service.getName() << std::endl;
+    for (std::string instance : instances)
     {
-        servus::Strings hosts = service.getInstances();
+        std::cout << "  Service name: " << instance << std::endl;
+        std::cout << "  Host: " << service.getHost(instance) << std::endl;
 
-        std::cout << "Service type: " << service.getName() << std::endl;
-        for (std::string host : hosts)
+        for (std::string key : service.getKeys(instance))
         {
-            std::cout << "  Service name: " << host << std::endl;
-            for (std::string key : service.getKeys())
-            {
-                std::cout << "  TXT: " << key << "=" << service.get(host, key)
-                          << std::endl;
-            }
+            std::cout << "  TXT: " << key << "=" << service.get(instance, key)
+                      << std::endl;
         }
     }
-
-    void test(const std::string& serviceType, const std::string& instanceName,
-              const std::string& UUID, uint32_t inPort)
-    {
-        const uint32_t port = inPort;
-        const std::string& tmpServiceType = serviceType;
-        const std::string& tmpInstanceName = instanceName;
-
-        std::cout << "Starting announce: service type: " << tmpServiceType
-                  << ", service name: " << tmpInstanceName << ", port: " << port
-                  << std::endl;
-        std::cout.flush();
-
-        try
-        {
-            servus::Servus service(tmpServiceType);
-        }
-        catch (const std::runtime_error& e)
-        {
-            std::cerr << "Bailing, no avahi on a Travis CI setup" << std::endl;
-            std::cerr << e.what() << std::endl;\
-            throw;
-        }
-
-        servus::Servus service(tmpServiceType);
-
-        service.set("priority", "1");
-        service.set("UUID", UUID);
-
-        assert(!service.isAnnounced());
-        assert(service.get("priority") == "1");
-        assert(service.get("UUID") == UUID);
-        assert(service.get(UUID) == std::string());
-
-        const servus::Servus::Result& result =
-                service.announce(port, tmpInstanceName);
-        assert(service.isAnnounced());
-
-        assert(service.getName() == tmpServiceType);
-
-        if (!servus::Servus::isAvailable())
-        {
-            // BOOST_CHECK_EQUAL gives a link error for Result::NOT_SUPPORTED
-            assert(result == servus::Servus::Result::NOT_SUPPORTED);
-            std::cerr << "Service is not available" << std::endl;
-            return;
-        }
-
-#ifdef SERVUS_USE_DNSSD
-        assert(servus::Result::SUCCESS == kDNSServiceErr_NoError);
-#endif
-        if (result != servus::Result::SUCCESS) // happens on CI VMs
-        {
-            std::cerr << "Bailing, got " << result
-                      << ": looks like a broken zeroconf setup" << std::endl;
-            return;
-        }
-
-        assert(result == result);
-        assert(result.getString() == "success");
-        assert(result.getCode() == 0);
-        std::cout << "  announce: " << result << std::endl; // for coverage
-
-        //changing message
-        std::this_thread::sleep_for(std::chrono::milliseconds(4000));
-        std::cout << "Changing message ..." << std::endl;
-        service.set("Test", "Daqri test");
-
-        //browse if data are in network.
-        std::this_thread::sleep_for(std::chrono::milliseconds(4000));
-        std::cout << "Browse results: " << std::endl;
-
-        // continuous browse API to browse results.
-        assert(!service.isBrowsing());
-        assert(service.beginBrowsing(servus::Servus::IF_LOCAL));
-        assert(service.isBrowsing());
-        // BOOST_CHECK_EQUAL gives a link error for Result::PENDING
-        assert(service.beginBrowsing(servus::Servus::IF_LOCAL) ==
-                    servus::Servus::Result::PENDING);
-        assert(service.isBrowsing());
-
-        assert(service.browse(_propagationTime) == service.browse(0));
-        servus::Strings hosts = service.getInstances();
-        assert(hosts.size() == 1);
-        assert(service.get(hosts.front(), "UUID") == UUID);
-        assert(service.getKeys().size() == 3);
-
-        printHosts(service);
-
-        service.withdraw();
-        std::cout << "End test" << std::endl;
-        std::cout.flush();
-    }
-
 }
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, this is announce server sample application!" << std::endl;
+void test(const std::string& serviceType, const std::string& instanceName,
+          const std::string& UUID, uint32_t inPort)
+{
+    const uint32_t port = inPort;
+    const std::string& tmpServiceType = serviceType;
+    const std::string& tmpInstanceName = instanceName;
+
+    std::cout << "Starting announce: service type: " << tmpServiceType
+              << ", service name: " << tmpInstanceName << ", port: " << port
+              << std::endl;
+    std::cout.flush();
+
+    try
+    {
+        servus::Servus service(tmpServiceType);
+    }
+    catch (const std::runtime_error& e)
+    {
+        std::cerr << "Bailing, no avahi on a Travis CI setup" << std::endl;
+        std::cerr << e.what() << std::endl;
+        throw;
+    }
+
+    servus::Servus service(tmpServiceType);
+
+    service.set("priority", "1");
+    service.set("UUID", UUID);
+
+    assert(!service.isAnnounced());
+    assert(service.get("priority") == "1");
+    assert(service.get("UUID") == UUID);
+    assert(service.get(UUID) == std::string());
+
+    const servus::Servus::Result& result =
+        service.announce(port, tmpInstanceName);
+    assert(service.isAnnounced());
+
+    assert(service.getName() == tmpServiceType);
+
+    if (!servus::Servus::isAvailable())
+    {
+        // BOOST_CHECK_EQUAL gives a link error for Result::NOT_SUPPORTED
+        assert(result == servus::Servus::Result::NOT_SUPPORTED);
+        std::cerr << "Service is not available" << std::endl;
+        return;
+    }
+
+#ifdef SERVUS_USE_DNSSD
+    assert(servus::Result::SUCCESS == kDNSServiceErr_NoError);
+#endif
+    if (result != servus::Result::SUCCESS) // happens on CI VMs
+    {
+        std::cerr << "Bailing, got " << result
+                  << ": looks like a broken zeroconf setup" << std::endl;
+        return;
+    }
+
+    assert(result == result);
+    assert(result.getString() == "success");
+    assert(result.getCode() == 0);
+    std::cout << "  announce: " << result << std::endl; // for coverage
+
+    // changing message
+    std::this_thread::sleep_for(std::chrono::milliseconds(4000));
+    std::cout << "Changing message ..." << std::endl;
+    service.set("Test", "Daqri test");
+
+    // browse if data are in network.
+    std::this_thread::sleep_for(std::chrono::milliseconds(4000));
+    std::cout << "Browse results: " << std::endl;
+
+    // continuous browse API to browse results.
+    assert(!service.isBrowsing());
+    assert(service.beginBrowsing(servus::Servus::IF_LOCAL));
+    assert(service.isBrowsing());
+    // BOOST_CHECK_EQUAL gives a link error for Result::PENDING
+    assert(service.beginBrowsing(servus::Servus::IF_LOCAL) ==
+           servus::Servus::Result::PENDING);
+    assert(service.isBrowsing());
+
+    assert(service.browse(_propagationTime) == service.browse(0));
+    servus::Strings hosts = service.getInstances();
+    assert(hosts.size() == 1);
+    assert(service.get(hosts.front(), "UUID") == UUID);
+    assert(service.getKeys().size() == 3);
+
+    printHosts(service);
+
+    service.withdraw();
+    std::cout << "End test" << std::endl;
+    std::cout.flush();
+}
+}
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello, this is announce server sample application!"
+              << std::endl;
 
     std::string serviceType = "_daqri-service._tcp";
     std::string UUID = std::to_string(servus::make_UUID());
@@ -151,7 +154,7 @@ int main(int argc, char **argv) {
     test(serviceType, serviceName, UUID, 2366);
 
 #ifdef _WIN32
-	getchar();
+    getchar();
 #endif
 
     return 0;
--- apps/SampleBrowser/SampleBrowser.cpp
+++ apps/SampleBrowser/SampleBrowser.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 
 #include <cassert>
-#include <chrono>         // std::chrono::seconds
-#include <thread>         // std::this_thread::sleep_for
+#include <chrono> // std::chrono::seconds
+#include <thread> // std::this_thread::sleep_for
 
 #include <servus/listener.h>
 #include <servus/servus.h>
@@ -21,123 +21,116 @@
 #define _sleep ::sleep
 #endif
 
-namespace {
+namespace
+{
+static const int _propagationTime = 1000;
+static const int _propagationTries = 50;
 
-    static const int _propagationTime = 1000;
-    static const int _propagationTries = 50;
+void printHosts(const servus::Servus& service);
+void test(const std::string& serviceType, const std::string& instanceName,
+          const std::string& UUID, uint32_t inPort);
 
-    void printHosts(const servus::Servus& service);
-    void test(const std::string& serviceType, const std::string& instanceName,
-              const std::string& UUID, uint32_t inPort);
+void printHosts(const servus::Servus& service)
+{
+    servus::Strings instances = service.getInstances();
 
-    void printHosts(const servus::Servus& service)
+    std::cout << "Service type: " << service.getName() << std::endl;
+    for (std::string instance : instances)
     {
-        servus::Strings hosts = service.getInstances();
+        std::cout << "  Service name: " << instance << std::endl;
+        std::cout << "  Host: " << service.getHost(instance) << std::endl;
 
-        std::cout << "Service type: " << service.getName() << std::endl;
-        for (std::string host : hosts)
+        for (std::string key : service.getKeys(instance))
         {
-            std::cout << "  Service name: " << host << std::endl;
-            for (std::string key : service.getKeys())
-            {
-                std::cout << "  TXT: " << key << "=" << service.get(host, key)
-                          << std::endl;
-            }
+            std::cout << "  TXT: " << key << "=" << service.get(instance, key)
+                      << std::endl;
         }
     }
-
-    void test(const std::string& serviceType)
-    {
-
-        const std::string& tmpServiceType = serviceType;
-
-        std::cout << "Starting browsing: service type: " << tmpServiceType << std::endl;
-        std::cout.flush();
-
-        try
-        {
-            servus::Servus service(tmpServiceType);
-        }
-        catch (const std::runtime_error& e)
-        {
-            std::cerr << "Bailing, no avahi on a Travis CI setup" << std::endl;
-            std::cerr << e.what() << std::endl;\
-            throw;
-        }
-
-        servus::Servus service(tmpServiceType);
-
-//        service.set("priority", "1");
-//        service.set("UUID", "0123456");
-//        const servus::Servus::Result& result =
-//                service.announce(2222, std::to_string(2222));
-
-        std::cout << "Discover results: " << std::endl;
-        int nLoops = _propagationTries;
-        while (--nLoops)
-        {
-
-            const servus::Strings& hosts = service.discover(servus::Servus::IF_ALL, _propagationTime);
-
-            std::cout << "... " << hosts.size() << std::endl;
-
-            const bool hasUUID =
-                    !hosts.empty() && service.containsKey(hosts.front(), "UUID");
-
-            if ((hosts.empty() || !hasUUID)){
-                continue;
-            }
-
-            assert(hosts.size() == 1);
-            assert(service.containsKey(hosts.front(), "priority"));
-            assert(service.containsKey(hosts.front(), "UUID"));
-            assert(service.get(hosts.front(), "priority") == "1");
-            assert(service.get("1", "priority").empty());
-            assert(service.get(hosts.front(), "Test").empty());
-
-            printHosts(service);
-            break;
-        }
-
-        //browse if data are in network.
-//        std::this_thread::sleep_for(std::chrono::milliseconds(4000));
-//        std::cout << "Browse results: " << std::endl;
-//
-//        // continuous browse API to browse results.
-//        assert(!service.isBrowsing());
-//        assert(service.beginBrowsing(servus::Servus::IF_LOCAL));
-//        assert(service.isBrowsing());
-//        // BOOST_CHECK_EQUAL gives a link error for Result::PENDING
-//        assert(service.beginBrowsing(servus::Servus::IF_LOCAL) ==
-//                    servus::Servus::Result::PENDING);
-//        assert(service.isBrowsing());
-//
-//        assert(service.browse(_propagationTime) == service.browse(0));
-//        servus::Strings hosts = service.getInstances();
-//        assert(hosts.size() == 1);
-//        assert(!service.get(hosts.front(), "UUID").empty());
-//        assert(service.getKeys().size() == 3);
-//
-//        printHosts(service);
-
-        service.withdraw();
-        std::cout << "End test" << std::endl;
-        std::cout.flush();
-    }
-
 }
 
-int main(int argc, char **argv) {
+void test(const std::string& serviceType)
+{
+    const std::string& tmpServiceType = serviceType;
+
+    std::cout << "Starting browsing: service type: " << tmpServiceType
+              << std::endl;
+    std::cout.flush();
+
+    try
+    {
+        servus::Servus service(tmpServiceType);
+    }
+    catch (const std::runtime_error& e)
+    {
+        std::cerr << "Bailing, no avahi on a Travis CI setup" << std::endl;
+        std::cerr << e.what() << std::endl;
+        throw;
+    }
+
+    servus::Servus service(tmpServiceType);
+
+    std::cout << "Discovering results: " << std::endl;
+    int nLoops = _propagationTries;
+    while (--nLoops)
+    {
+        const servus::Strings& hosts =
+            service.discover(servus::Servus::IF_ALL, _propagationTime);
+
+        const bool hasUUID =
+            !hosts.empty() && service.containsKey(hosts.front(), "UUID");
+
+        if ((hosts.empty() || !hasUUID))
+        {
+            std::cout << "... " << std::endl;
+            continue;
+        }
+
+        assert(hosts.size() == 1);
+        assert(service.containsKey(hosts.front(), "priority"));
+        assert(service.containsKey(hosts.front(), "UUID"));
+        assert(service.get(hosts.front(), "priority") == "1");
+        assert(service.get("1", "priority").empty());
+
+        printHosts(service);
+        //        break;
+    }
+
+    // browse if data are in network.
+    //        std::this_thread::sleep_for(std::chrono::milliseconds(4000));
+    //        std::cout << "Browse results: " << std::endl;
+    //
+    //        // continuous browse API to browse results.
+    //        assert(!service.isBrowsing());
+    //        assert(service.beginBrowsing(servus::Servus::IF_LOCAL));
+    //        assert(service.isBrowsing());
+    //        // BOOST_CHECK_EQUAL gives a link error for Result::PENDING
+    //        assert(service.beginBrowsing(servus::Servus::IF_LOCAL) ==
+    //                    servus::Servus::Result::PENDING);
+    //        assert(service.isBrowsing());
+    //
+    //        assert(service.browse(_propagationTime) == service.browse(0));
+    //        servus::Strings hosts = service.getInstances();
+    //        assert(hosts.size() == 1);
+    //        assert(!service.get(hosts.front(), "UUID").empty());
+    //        assert(service.getKeys().size() == 3);
+    //
+    //        printHosts(service);
+
+    std::cout << "End test" << std::endl;
+    std::cout.flush();
+}
+}
+
+int main(int argc, char** argv)
+{
     std::cout << "Hello, this is sample browser application! \n" << std::endl;
 
     std::string serviceType = "_daqri-service._tcp";
-//    std::string UUID = std::to_string(servus::make_UUID());
-//    std::string serviceName = "daqri.core." + UUID;
 
     test(serviceType);
 
 #ifdef _WIN32
-	getchar();
+    getchar();
 #endif
 
     return 0;
